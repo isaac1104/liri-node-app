db462343f39d4069217adace3e8d40b76e3218ce
Isaac Kwon <you@example.com>
2017-08-23 23:23:57 -0700

    3rd Commit: Working App

--- liri.js
+++ liri.js
@@ -1,8 +1,18 @@
+var fs = require("fs");
 var keys = require("./key");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
+var nodeArg = process.argv;
 var userRequest = process.argv[2];
-var value = process.argv[3];
+var value = "";
+
+for (var i = 3; i < nodeArg.length; i++) {
+  if (i > 3 && i < nodeArg.length) {
+    value = value + "+" + nodeArg[i];
+  } else {
+    value += nodeArg[i];
+  }
+}
 
 if (userRequest === "movie-this") {
   movieData(value);
@@ -14,7 +24,9 @@ if (userRequest === "movie-this") {
 
 //Get data from twitter
 function twitterData() {
-  var myTweets = { screen_name: "isaac3532"};
+  var myTweets = {
+    screen_name: "muse"
+  };
   var client = new Twitter(keys.twitterKeys);
   client.get("statuses/user_timeline", myTweets, function(error, tweets, response) {
     if (!error && response.statusCode === 200) {
@@ -28,16 +40,18 @@ function twitterData() {
 //Get movie data from OMDB
 function movieData(value) {
   var request = require("request");
+
   if (value === "" || value === undefined) {
     value = "Mr.Nobody";
   }
+
   var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece";
   request(queryUrl, function(error, response, body) {
     var movieData = JSON.parse(body);
     if (!error && response.statusCode === 200) {
       console.log("Title: " + movieData.Title);
       console.log("Release Year: " + movieData.Year);
-      console.log("IMDB Rating: " + movieData.Ratings[0].Value);
+      console.log("IMDB Rating: " + movieData.imdbRating);
       console.log("Rotten Tomatoes Rating: " + movieData.Ratings[1].Value);
       console.log("Produced In :" + movieData.Country);
       console.log("Language: " + movieData.Language);
@@ -53,5 +67,21 @@ function songData(value) {
     id: "7203388ac6904e96a4dc5c0408e3373b",
     secret: "af2b85c8648c435a8a609b33d52b2b0d"
   });
-  console.log(spotify);
+
+  if (value === "" || value === undefined) {
+    value = "The Sign";
+  }
+
+  spotify.search({
+      type: 'track',
+      query: value + "&limit=1&",
+    },
+    function(err, data) {
+      if (!err) {
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Title: " + data.tracks.items[0].name);
+        console.log("Preview URL: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+      }
+    });
 }
